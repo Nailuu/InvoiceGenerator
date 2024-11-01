d43df9fcd1a302d51e9761e1b4cb582104e85cdd
Nailuu <contact@nailu.fr>
2024-11-01 17:18:13 +0100

    Setup application using Spring Boot

---
+++ src/main/java/fr/nailu/invoicegenerator/InvoiceGeneratorApplication.java
@@ -0,0 +1,15 @@
+package fr.nailu.invoicegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGeneratorApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(InvoiceGeneratorApplication.class, args);
+    }
+
+}
